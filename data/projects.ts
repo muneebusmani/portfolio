7ef5e0c948c7b83a808daa47913480620fed8f25
muneeb <muneebusmani8355@gmail.com>
2025-05-11 02:59:45 +0500

    feat: more links in projects

--- data/projects.ts
+++ data/projects.ts
@@ -28,43 +28,46 @@ export const projects: Project[] = [
     description:
       "Developed a responsive multi-page website for a law firm using PHP, JavaScript, and CSS. Features included service listings, contact form, and firm details. Focused on clean UI, accessibility, and structured backend integration.",
     tech: ["PHP", "MYSQL"],
-    githubLink: "https://github.com/muneebusmani/Lawyers-Website",
+    liveLink: "https://justicialaw.muneebusmani.com",
+    githubLink: "https://github.com/muneebusmani/Project",
   },
 
-  {
-    title: "Nexus Communication Website",
-    description:
-      "The Nexus Communication System is a role-based enterprise application designed to streamline operations for a telecommunications company. Built using C# and ASP.NET MVC, it features a structured architecture with distinct panels tailored for customers, administrators, and accounts personnel.",
-    tech: ["C#", "ASP.NET", "Bootstrap", "MSSQL", "EF Core"],
-    githubLink: "https://github.com/muneebusmani/Nexus_Communication-eproject-",
-  },
+  // {
+  //   title: "Nexus Communication Website",
+  //   description:
+  //     "The Nexus Communication System is a role-based enterprise application designed to streamline operations for a telecommunications company. Built using C# and ASP.NET MVC, it features a structured architecture with distinct panels tailored for customers, administrators, and accounts personnel.",
+  //   tech: ["C#", "ASP.NET", "Bootstrap", "MSSQL", "EF Core"],
+  //   githubLink: "https://github.com/muneebusmani/Nexus_Communication-eproject-",
+  // },
   {
     title: "Learn Skills",
     description:
       "A Learning Management System, Written in Next.js(migrated from static html, css into fullstack application in nextjs)",
     tech: ["Next.JS", "Typescript", "Bootstrap"],
     liveLink: "https://learnskills.muneebusmani.com",
-    githubLink: "https://github.com/muneebusmani/learnskills",
+    githubLink: "https://github.com/muneebusmani/learnskills/tree/nextjs",
   },
+  {
+    title: "Learn Skills(Static)",
+    description: "A Learning Management System, Static Website",
+    tech: ["Html", "CSS", "JavaScript", "JQuery", "Bootstrap"],
+    liveLink: "https://old-learnskills.netlify.app",
+    githubLink: "https://github.com/muneebusmani/learnskills/tree/master",
+  },
+
   {
     title: "Olink(Digital Agency Website)",
     description:
-      "A Digital Agency website written in pure html, css and bootstrap. migrated into Nextjs, Typescript for better performance",
-    tech: ["Next.JS", "Typescript", "Bootstrap"],
+      "A Digital Agency website written in pure html, css and bootstrap. migration into Nextjs is in progress, Typescript for better performance",
+    tech: ["Html", "css", "JQuery", "Bootstrap"],
     liveLink: "https://olink.muneebusmani.com",
     githubLink: "https://github.com/muneebusmani/olink",
   },
   {
     title: "Todo List",
     description: "A Todo App Built in Laravel blade with user based notes.",
-    tech: ["Laravel", "Blade", "MySQL", "PHP"],
+    tech: ["Laravel", "Blade", "SQLite"],
+    liveLink: "https://todolist.muneebusmani.com",
     githubLink: "https://github.com/muneebusmani/laravel-todolist",
-  },
-  {
-    title: "Todo Cli",
-    description:
-      "A CLI Based, Cross Platform, Clickup like Task management App written in Python",
-    tech: ["Python"],
-    githubLink: "https://github.com/muneebusmani/todocli",
   },
 ];
